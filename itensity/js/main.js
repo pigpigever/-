c6b661f27ba711bf8cd096a668657d0f5015d8a5
pigpigever <1690384523@qq.com>
2017-09-25 01:19:25 +0800

    完成侧边栏效果

--- itensity/js/main.js
+++ itensity/js/main.js
@@ -1,12 +1,21 @@
 
 $(function () {
 
-    $("#header nav a[href='#menu']").click(function () {
-         $("body").addClass("toggled")
-         console.log($("#menu").attr("id"))
-    })
-
-    $("#menu > div:nth-child(1)").click(function(){
+    $("body").click(function(){
         $("body").removeClass("toggled")
     })
+
+    $("#header nav a[href='#menu']").click(function () {
+        event.stopPropagation() //阻止冒泡
+         $("body").addClass("toggled")
+    })
+
+    $("#menu").click(function(){
+        event.stopPropagation() //阻止冒泡
+    })
+
+    $("#menu > div:nth-child(1)").click(function(event){
+        $("body").removeClass("toggled")
+    })
+
 })
