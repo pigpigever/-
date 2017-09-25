00ee0a95cb9dc5b3bc374a70010af64a649d65cf
pigpigever <1690384523@qq.com>
2017-09-25 21:21:26 +0800

    增加banner动画并修改在屏幕宽度缩小时导航条的bug

--- itensity/js/main.js
+++ itensity/js/main.js
@@ -1,21 +1,34 @@
 
 $(function () {
 
-    $("body").click(function(){
+    //初始化
+    init()
+
+}())
+
+function init() {
+    $("body").addClass("is-loading")
+    $(window).on("load", function () {
+        window.setTimeout(function () {
+            $("body").removeClass("is-loading")
+        }, 100)
+    })
+
+    /**初始化侧边栏动画 */
+    $("body").click(function () {
         $("body").removeClass("toggled")
     })
 
     $("#header nav a[href='#menu']").click(function () {
         event.stopPropagation() //阻止冒泡
-         $("body").addClass("toggled")
+        $("body").addClass("toggled")
     })
 
-    $("#menu").click(function(){
+    $("#menu").click(function () {
         event.stopPropagation() //阻止冒泡
     })
 
-    $("#menu > div:nth-child(1)").click(function(event){
+    $("#menu > div:nth-child(1)").click(function (event) {
         $("body").removeClass("toggled")
     })
-
-})
+}
